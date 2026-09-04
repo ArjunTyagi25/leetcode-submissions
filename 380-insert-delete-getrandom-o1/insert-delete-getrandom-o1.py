767e168e8393c25803ba4e20b510a74beca8b3b1
Arjun Tyagi <arjun25@illinois.edu>
2026-09-04 06:47:53 -0500

    Time: 116 ms (45.14%) | Memory: 57.2 MB (10.04%) - LeetSync

--- 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
@@ -1,6 +1,7 @@
 class RandomizedSet:
 
     def __init__(self):
+        self.values = []
         self.map = {}
         
 
@@ -8,7 +9,8 @@ class RandomizedSet:
         if val in self.map:
             return False
         
-        self.map[val] = -1
+        self.values.append(val)
+        self.map[val] = len(self.values)-1
         return True
         
 
@@ -16,12 +18,19 @@ class RandomizedSet:
         if val not in self.map:
             return False
 
+        index_of_val_remove = self.map[val]
+        t = self.values[-1]
+        self.values[-1] = val
+        self.values[index_of_val_remove] = t
+
+        self.map[t] = index_of_val_remove
+        self.values.pop()
         del self.map[val]
         return True
         
 
     def getRandom(self) -> int:
-        val_to_return = random.choice([k for k in self.map.keys()])
+        val_to_return = random.choice(self.values)
 
         return val_to_return
         
