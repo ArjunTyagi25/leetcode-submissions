f1984d756ba5d763d4cb664aa94d9b31979723df
Arjun Tyagi <arjun25@illinois.edu>
2026-09-06 13:03:27 -0500

    Time: 159 ms (5.07%) | Memory: 36.4 MB (5.02%) - LeetSync

---
+++ 1305-number-of-visible-people-in-a-queue/number-of-visible-people-in-a-queue.py
@@ -0,0 +1,34 @@
+class Solution:
+    def canSeePersonsCount(self, heights: List[int]) -> List[int]:
+        '''
+        Two conditions:
+        1. If top of stack > heights[i], then increment res for top of stack and push current index to stack
+        2. If top of stack < heights[i], then until top of stack becomes larger again, increment res for top of stack and pop it. Ultimately push the current index to the stack
+        '''
+        
+        stack = []
+        res = [0] * len(heights)
+
+        for i in range(len(heights)):
+            if stack == []:
+                stack.append([heights[i], i])
+            else:
+                if stack[-1][0] > heights[i]:
+                    res[stack[-1][1]] += 1
+                    stack.append([heights[i], i])
+                else:
+                    while stack and stack[-1][0] < heights[i]:
+                        res[stack[-1][1]] += 1
+                        stack.pop()
+
+                    if stack:
+                        res[stack[-1][1]] += 1
+                    stack.append([heights[i], i])
+        
+        return res
+
+
+
+        return res
+
+        
