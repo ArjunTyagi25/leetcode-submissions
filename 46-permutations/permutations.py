33fddfb9355caa21780aac16191700eb5c5d2847
Arjun Tyagi <arjun25@illinois.edu>
2026-09-14 17:11:45 -0500

    Time: 0 ms (100.00%) | Memory: 19.6 MB (36.03%) - LeetSync

---
+++ 46-permutations/permutations.py
@@ -0,0 +1,20 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        self.res = []
+        visited = set()
+        
+        def rec(curr_perm):
+            if len(curr_perm) == len(nums):
+                self.res.append(curr_perm.copy())
+                return
+
+            for num in nums:
+                if num not in visited:
+                    curr_perm.append(num)
+                    visited.add(num)
+                    rec(curr_perm)
+                    curr_perm.pop()
+                    visited.remove(num)
+
+        rec([])
+        return self.res
